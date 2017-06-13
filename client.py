3575612f46181a21969c5e63a3fe8a029e37c5ff
sisekoreggie <3443307@myuwc.ac.za>
2017-06-13 19:20:34 +0200

    Limited the chat to exactly two people

--- client.py
+++ client.py
@@ -1,17 +1,50 @@
+# telnet program example
+import socket, select, string, sys
+ 
+def prompt() :
+    sys.stdout.write('<You> ')
+    sys.stdout.flush()
+ 
+#main function
+if __name__ == "__main__":
 
-from socket import *
- 
-host = 'localhost' # '127.0.0.1' can also be used
-port = 52000
- 
-sock = socket()
-#Connecting to socket
-sock.connect((host, port)) #Connect takes tuple of host and port
- 
-#Infinite loop to keep client running.
-while True:
-    data = sock.recv(1024)
-    print data
-    sock.send('HI! I am client.')
- 
-sock.close()
+    host = "localhost"
+    port = 5000
+     
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+     
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Unable to connect'
+        sys.exit()
+     
+    print 'Connected to remote host. Start sending messages'
+    prompt()
+     
+    while 1:
+        socket_list = [sys.stdin, s]
+         
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+         
+        for sock in read_sockets:
+            #incoming message from remote server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDisconnected from chat server'
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write(data)
+                    prompt()
+             
+            #user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
+
